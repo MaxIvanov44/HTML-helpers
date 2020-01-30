007e414edc7467492e54c2f17a8a88e33d1333cb
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-30 17:42:58 +0300

    helpers

---
+++ HTML helpers/HTML helpers/App_Code/ListHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace HTML_helpers.App_Code
+{
+    public static class ListHelper
+    {
+        public static HtmlString CreateList(this IHtmlHelper html, string[] items)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+            foreach (string item in items)
+            {
+                TagBuilder li = new TagBuilder("li");
+                // добавляем текст в li
+                li.InnerHtml.Append(item);
+                // добавляем li в ul
+                ul.InnerHtml.AppendHtml(li);
+            }
+            ul.Attributes.Add("class", "itemsList");
+            var writer = new System.IO.StringWriter();
+            ul.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+    public enum TimeOfDay
+    {
+        [Display(Name = "Утро")]
+        Morning,
+        [Display(Name = "День")]
+        Afternoon,
+        [Display(Name = "Вечер")]
+        Evening,
+        [Display(Name = "Ночь")]
+        Night
+    }
+}
---
+++ HTML helpers/HTML helpers/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HTML_helpers.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HTML_helpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult GetUser(string name = "Тоm", int id = 1)
+        {
+            return Content($"{id}. {name}");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            return Content($"{user.Name} - {user.Age}");
+        }
+
+        public IActionResult Details()
+        {
+            User tom = new User { Id = 1, Name = "Tom", Age = 35 };
+            return View(tom);
+        }
+
+       
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
